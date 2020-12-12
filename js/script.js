ac45d700535edd1f8f964410f510927c3e253d4f
ivan.n <ivan.n@it-dimension.com>
2020-12-12 23:42:30 +0300

    1.0.0

---
+++ js/script.js
@@ -0,0 +1,60 @@
+window.onload = async (event) => {
+    let loader = document.getElementById('loader')
+    let content = document.querySelector('.wrapper')
+
+    content.classList.add('active')
+    loader.remove()
+};
+async function copyMail() {
+
+    let promise = await navigator.clipboard.writeText("i.ndeilko@gmail.com")
+    iziToast.show({
+        progressBar: false,
+        timeout: 3000,
+        zindex: 3,
+        theme: 'dark',
+        messageColor: 'white',
+        messageSize: '18',
+        color: 'green',
+        position: 'topRight',
+        message: 'Email copied to clipboard'
+    });
+}
+const scrollToHome = () => {
+    let elem = document.querySelector('.container-preview')
+
+    window.scroll({
+        left: 0,
+        top: elem.offsetTop,
+        behavior: 'smooth'
+    })
+}
+const scrollToAboutMe = () => {
+    let elem = document.querySelector('.about-me-container')
+    console.log(elem)
+    window.scroll({
+        left: 0,
+        top: elem.offsetTop,
+        behavior: 'smooth'
+    })
+}
+const scrollToProjects = () => {
+    let elem = document.querySelector('.container-projects')
+
+    window.scroll({
+        left: 0,
+        top: elem.offsetTop,
+        behavior: 'smooth'
+    })
+}
+const scrollToContacts = () => {
+    let elem = document.querySelector('footer')
+
+    window.scroll({
+        left: 0,
+        top: elem.offsetTop,
+        behavior: 'smooth'
+    })
+}
+
+
